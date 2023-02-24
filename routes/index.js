311e80cf20c02fb862da05f96a438d8872b8e602
Hongmin <hongmin@Hongminui-MacBookPro.local>
2023-02-24 13:36:29 +0900

    [fix] title

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ const router = express.Router()
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'AINFT Tutorial Server' })
+  res.render('index', { title: 'AINFT A DAY' })
 })
 
 module.exports = router
